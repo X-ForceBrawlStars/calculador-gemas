a4fe827cb93bbe28ba106215a3e35807024c5a83
X-ForceBrawlStars <xforcebrasilstars@gmail.com>
2026-06-05 23:21:05 -0300

    Adicionando popup para vizualização detalhada de dados específicos 	- popup abrindo em card com fundo translúcido, ao apertar fora do popup é possivel fecha-lo, 	- no momento é possivel transicionar entre 3 telas diferentes no popup.

--- javascript/script.js
+++ javascript/script.js
@@ -6,26 +6,47 @@ const skinLendaria = document.getElementById("skinLendaria");
 const skinHyper = document.getElementById("skinHyper");
 const skinPrateada = document.getElementById("skinPrateada");
 const skinDourada = document.getElementById("skinDourada");
+
 const emojiComum = document.getElementById("emojiComum");
 const emojiRaro = document.getElementById("emojiRaro");
 const emojiEpico = document.getElementById("emojiEpico");
+
 const icone = document.getElementById("icone");
 const gadget = document.getElementById("gadget");
 const starPower = document.getElementById("starPower");
 const buffies = document.getElementById("buffies");
 const hyper = document.getElementById("hyper");
+
 const btnCalcular = document.getElementById("calcular");
 const resultado = document.getElementById("resultado");
+
 const gemasSkin = [29, 79, 149, 199, 299, 499, 1000, 2500];
 const skins = [skinRara, skinSuperRara, skinEpica, skinMitica,
      skinLendaria, skinHyper, skinPrateada, skinDourada];
 const gemasEmoji = [10, 30, 50];
 const emojis = [emojiComum, emojiRaro, emojiEpico];
+const emojisValor = [];
 const skinsValor = [];
 const contBrawler = document.getElementById("contBrawler");
 const niveisBrawlers = document.getElementById("niveisBrawlers"); 
 const valoresBrawler = [424, 276.5, 180, 114, 69, 42, 22, 12.5, 6.5, 4, 0];
 const nomesSkins = ["Rara", "Super Rara", "Épica", "Mítica", "Lendária", "Hyper", "Prateada", "Dourada"];
+const nomesEmojis = ["Comum", "Raro", "Épico"];
+
+const btnDetalhes = document.getElementById("btnDetalhes");
+const popup = document.getElementById("popup");
+const paginaDetalhes = document.getElementById("paginaDetalhes");
+const btnAnterior = document.getElementById("btnAnterior");
+const btnProximo = document.getElementById("btnProximo");
+const btnFechar = document.getElementById("btnFechar");
+const contadorPagina = document.getElementById("contadorPagina");
+
+let paginaAtual = 0;
+
+let paginas = [];
+let htmlSkins = "<h3>Detalhes das skins</h3>";
+let htmlBrawlers = "<h3>Detalhes dos brawlers</h3>";
+let htmlEmojis = "<h3>Detalhes dos emojis</h3>";
 
 function Calcular(){
     console.log("Clicou");
@@ -59,6 +80,12 @@ function Calcular(){
         gemasNivel[i] = niveis[i] * valoresBrawler[i];
     }
 
+    for(let i = 0; i < gemasNivel.length; i++){
+        const nvl = 11 - i;
+
+        htmlBrawlers += `<p>Nível ${nvl}: ${gemasNivel[i]} gemas</p>`;
+    }
+
     console.log("Variavel niveis: ", niveis);
     console.log("niveis length: ", niveis.length);
     console.log(brawlerValor);
@@ -77,10 +104,20 @@ function Calcular(){
         skinsValor[i] = Number(skins[i].value) * gemasSkin[i];
     }
 
+    for(let i = 0; i < nomesSkins.length; i++){
+        console.log("entrou no loop");
+        htmlSkins += `<p>${nomesSkins[i]}(s) valem: ${skinsValor[i]} gemas </p>`;
+    }
+
     let totalGemasEmoji = 0;
 
     for(let i = 0; i < emojis.length; i++){
         totalGemasEmoji += Number(emojis[i].value) * gemasEmoji[i];
+        emojisValor[i] = Number(emojis[i].value) * gemasEmoji[i];
+    }
+
+    for(let i = 0; i < nomesEmojis.length; i++){
+        htmlEmojis += `<p>${nomesEmojis[i]}(s): ${emojisValor[i]} gemas`;
     }
 
     const valorConta = totalGemasSkins + totalGemasEmoji +
@@ -88,7 +125,6 @@ function Calcular(){
 
     resultado.innerHTML = `
     <h3>Seus dados</h3> 
-    <br>
     <p>Suas skins valem: ${totalGemasSkins.toLocaleString("pt-BR", {minimumFractionDigits: 0, maximumFractionDigits: 0})} gemas</p>
     <p>Seus emojis valem: ${totalGemasEmoji.toLocaleString("pt-BR", {minimumFractionDigits: 0, maximumFractionDigits: 0})} gemas</p>
     <p>Icones valem: ${valorIcone.toLocaleString("pt-BR", {minimumFractionDigits: 0, maximumFractionDigits: 0})} gemas</p> 
@@ -100,6 +136,10 @@ function Calcular(){
     <p>Seus cosméticos, itens e progresso da sua conta equivalem a: ${valorConta.toLocaleString("pt-BR", {minimumFractionDigits: 0, maximumFractionDigits: 0})} gemas</p>
     
     `;
+
+    paginas = [htmlSkins, htmlBrawlers, htmlEmojis];
+
+    btnDetalhes.style.display = "flex";
 }
 
 function GerarCampos(){
@@ -115,7 +155,65 @@ function GerarCampos(){
     }
 }
 
+function AbrirPopup(){
+    paginaAtual = 0;
+
+    AtualizarPagina();
+
+    popup.style.display = "flex";
+
+    setTimeout(() => {
+        popup.classList.add("ativo");
+    }, 10);
+}
+
+function FecharPopup(){
+    
+    popup.classList.remove("ativo");
+
+    setTimeout(() => {
+        popup.style.display = "none";
+    }, 300);
+}
+
+function AtualizarPagina(){
+    paginaDetalhes.innerHTML = paginas[paginaAtual];
+
+    contadorPagina.textContent = `${paginaAtual + 1}/${paginas.length}`;
+}
+
+function ProximaPagina(){
+    if(paginaAtual < paginas.length - 1){
+        paginaAtual++;
+
+        AtualizarPagina();
+    }
+}
+
+function PaginaAnterior(){
+    if(paginaAtual > 0){
+        paginaAtual--;
+
+        AtualizarPagina();
+    }
+}
+
+
+
+
+
+
 btnCalcular.addEventListener("click", Calcular);
+btnDetalhes.addEventListener("click", AbrirPopup);
+btnFechar.addEventListener("click", FecharPopup);
+btnProximo.addEventListener("click", ProximaPagina);
+btnAnterior.addEventListener("click", PaginaAnterior);
+popup.addEventListener("click", function(event){
+    if(event.target == popup){
+        FecharPopup();
+    }
+});
+
 
 GerarCampos();
 
